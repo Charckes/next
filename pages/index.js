77c0fe77588a0d30c1219f6dc7221893814bac05
Charckes <charlesmochss@gmail.com>
2023-03-18 15:32:52 -0300

    teste de Link

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import { useState } from "react"
 
+import Link from 'next/link'
+
 function Home() {
     return (
         <div>
-            <p>Home</p>
-            <Contador />
+            <h1>Home</h1>
+            <Link href="/sobre">
+                <a>Acessar página sobre</a>
+            </Link>
         </div>
     )
 }
